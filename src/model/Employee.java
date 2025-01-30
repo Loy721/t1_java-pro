8ca3f89b6f1f62dc54d206bb61e92a7136c043b7
Долгих Данила Юрьевич <danil.dolgikh@simbirsoft.com>
2025-01-30 15:54:31 +0300

    Add solve

---
+++ src/model/Employee.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Employee {
+    private String name;
+    private int age;
+    private String post;
+
+    public Employee(String name, int age, String post) {
+        this.name = name;
+        this.age = age;
+        this.post = post;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", post='" + post + '\'' +
+                '}';
+    }
+}
